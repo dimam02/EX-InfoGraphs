8d99b7b54cfeba7fa10a96c06d727177b7f63116
dimam02 <109684406+dimam02@users.noreply.github.com>
2023-06-14 03:57:27 +0300

    Trajectory

--- Advanced_Satellites_Data.py
+++ Advanced_Satellites_Data.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Assuming you have a CSV file named 'UCS-Satellite-Database-5-1-2022 - Sheet1.csv' with columns 'Year' and 'Type'
+# Assuming you have a CSV file named 'UCS-Satellite-Database-5-1-2022 - Sheet1.csv' with columns 'Date of Launch' and 'Type'
 # Load the dataset
 df = pd.read_csv('/Users/Dmytro/Downloads/UCS-Satellite-Database-5-1-2022 - Sheet1.csv')
 
 
 # Group the data by year and count the number of satellites of each type
-satellite_counts = df.groupby(['Year', 'Type']).size().unstack(fill_value=0)
+satellite_counts = df.groupby(['Date of Launch', 'Type']).size().unstack(fill_value=0)
 
 # Plotting the data
 fig, ax = plt.subplots(figsize=(10, 6))
 satellite_counts.plot(kind='bar', stacked=True, ax=ax)
 
 # Set labels and title
-ax.set_xlabel('Year')
+ax.set_xlabel('Date of Launch')
 ax.set_ylabel('Number of Satellites')
 ax.set_title('Types of Satellites Launched Each Year')
 
